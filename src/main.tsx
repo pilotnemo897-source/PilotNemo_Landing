135c618c9eced1ba587f32ac0e73fcaed638191f
Thirumalaivasudhevan <thirumalaivasudhavan@gmail.com>
2026-08-25 14:04:57 +0530

    feat: redirect to home page on browser refresh from any route

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,22 @@ import App from './App'
 import NotFound from './pages/NotFound'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+// On page refresh, always redirect to home page
+const navEntries = performance.getEntriesByType('navigation') as PerformanceNavigationTiming[]
+const isReload = navEntries.length > 0 && navEntries[0].type === 'reload'
+
+if (isReload && window.location.pathname !== '/') {
+  window.location.replace('/')
+} else {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>,
+  )
+}
+
